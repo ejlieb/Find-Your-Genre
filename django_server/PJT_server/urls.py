b718f3323c7bb45f30d1b1af397c2133b308a0d9
suhpysiss <suhpysisssw@gmail.com>
2022-05-19 14:35:44 +0900

    restore_django

--- django_server/PJT_server/urls.py
+++ django_server/PJT_server/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movies.urls')),
+    path('api/v1/movies/', include('movies.urls')),
+    path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/communities/', include('communities.urls')),
 ]
--- django_server/communities/views.py
+++ django_server/communities/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
