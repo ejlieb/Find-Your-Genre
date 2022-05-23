5c2fa70cb1cb37bfe4bfab1438799eced7dd3f19
suhpysiss <suhpysisssw@gmail.com>
2022-05-23 15:37:39 +0900

    django_movie_detail_and_review_eval

--- final-pjt-back/communities/urls.py
+++ final-pjt-back/communities/urls.py
@@ -6,5 +6,7 @@ app_name = 'communities'
 
 urlpatterns = [
     path('<int:movie_id>/review_create/', views.review_create),
+    path('<int:movie_id>/<int:review_pk>/', views.review_detail),
+    path('<int:movie_id>/<int:review_pk>/good/', views.review_good),
     
 ]
