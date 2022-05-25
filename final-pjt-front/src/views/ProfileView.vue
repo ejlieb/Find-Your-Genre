40adfe67d91c84d63c19d5153136f2a58110f8e5
ejlieb <coryonronda@gmail.com>
2022-05-25 14:53:36 +0900

    vue ing

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -2,8 +2,28 @@
   <div class="container">
     <div class="for-nav"></div>
     <div class="profile-box">
-      <h1>{{ profile.username }}</h1>
       <p>{{profile}}</p>
+      <div>
+        <h1>{{ profile.username }}님의 프로필 페이지</h1>
+        <div class="row">
+          <div class="col-3">
+            <h1>좋아하는 장르</h1>
+          </div>
+           <div class="liked-movie-box d-flex flex-column col-9">
+              <div class="d-flex">
+                <p class="liked-movie-p align-self-start mx-3">Favorite Actors</p>
+              </div>
+            
+              <div class="liked-movie-img-box d-flex justify-content-center align-items-center">
+                <div v-for=" (movie,index) in slicedMovieList" :key="index" class="liked-movie-img mx-3" :id="`movie-${index}`">
+                  <!-- 클릭하면 영화 상세페이지로 가게 하기 -->
+                  <img :src="poster_path + movie.poster_path" alt="" class="movie-img" @click="goToDetail(movie)">
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+      
     <!-- <h1>{{ profile}}</h1> -->
     </div>
     
@@ -78,6 +98,9 @@ import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'ProfileView',
+  components: {
+
+  },
   data : function() {
     return{
       poster_path: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/',
@@ -88,6 +111,9 @@ export default {
     slicedMovieList: function() {
       const reversedList = [...this.profile.movie_likes].reverse()
       return reversedList.slice(0,8)
+    },
+    slicedActorList: function() {
+      return this.profile.actor_counts
     }
   },
   methods: {
