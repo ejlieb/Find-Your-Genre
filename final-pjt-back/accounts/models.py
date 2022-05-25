007a20ff6bdb4cca0ce27c8c1d8e8fbe387e09af
suhpysiss <suhpysisssw@gmail.com>
2022-05-25 15:24:28 +0900

    django_review_update

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -27,9 +27,3 @@ class ActorCounts(models.Model):
     counted_actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
     actor_cnt = models.IntegerField(default=0)
 
-
-# signin의 과정은 어떻게 진행될까?
-# Vue Client에게 API 요청을 받아 user_serializer를 전송함
-# user_serializer를 전송받은 Vue는 화면을 적절하게 렌더링하여 방문자에게 제공
-# 방문자는 Vue가 렌더링해준 페이지를 채워서 전송해줌
-# 
