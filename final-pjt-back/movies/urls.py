19c8cc2b15cf5b84b52605a53f905ad99056d445
suhpysiss <suhpysisssw@gmail.com>
2022-05-26 15:33:41 +0900

    django_recommend_algorithm

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     # path('actor/', views.actor_register),
     # path('movie_detail_register/', views.movie_imdb_register),
     # path('movie_images_register/', views.movie_images_register),
-    # path('test/', views.test)
+    path('test/<int:movie_id>/', views.test)
 ]
